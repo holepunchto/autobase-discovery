5b1cc10ed0c0ab5b2df1860f5d8aee5762620737
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-03-14 12:55:49 +0100

    Add bins + example (#4)
    * Add bins + example
    
    * Example without example key

---
+++ example.js
@@ -0,0 +1,20 @@
+const Hyperswarm = require('hyperswarm')
+const IdEnc = require('hypercore-id-encoding')
+const RegisterClient = require('./client/register')
+
+async function main () {
+  const rpcServerKey = '<Enter rpc-discovery RPC-server public key here>'
+  const swarm = new Hyperswarm()
+  await swarm.listen()
+  const myServerKey = swarm.keyPair.publicKey
+
+  const client = new RegisterClient(rpcServerKey, swarm.dht)
+  console.log(`Registering service ${IdEnc.normalize(myServerKey)}...`)
+  await client.putService(myServerKey)
+  console.log(`Registered ${IdEnc.normalize(myServerKey)}`)
+
+  await client.close()
+  await swarm.destroy()
+}
+
+main()
