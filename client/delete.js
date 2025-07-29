33cb40e2d356dd2e8c4f2aa3e661ec8736428ccb
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-07-29 13:50:01 +0200

    Use protomux-rpc-client v2 (#13)
    * Use protomux-rpc-client v2
    
    * backwards compat prop names

--- client/delete.js
+++ client/delete.js
@@ -1,24 +1,36 @@
-const c = require('compact-encoding')
+const cenc = require('compact-encoding')
 const IdEnc = require('hypercore-id-encoding')
 const ProtomuxRpcClient = require('protomux-rpc-client')
 const HyperDHT = require('hyperdht')
 
 const { resolveStruct } = require('../spec/hyperschema')
+const ReadyResource = require('ready-resource')
 const DeleteServiceRequest = resolveStruct('@autodiscovery/delete-service-request')
 
-class RpcDiscoveryDeleteClient extends ProtomuxRpcClient {
-  constructor (serverKey, dht, accessSeed, opts = {}) {
-    const keyPair = HyperDHT.keyPair(IdEnc.decode(accessSeed))
+class RpcDiscoveryDeleteClient extends ReadyResource {
+  constructor (serverKey, dht, accessSeed) {
+    super()
 
-    super(serverKey, dht, { ...opts, keyPair })
+    this.keyPair = HyperDHT.keyPair(IdEnc.decode(accessSeed))
+    this.rpcClient = new ProtomuxRpcClient(dht, { keyPair: this.keyPair })
+    this.serverKey = IdEnc.decode(serverKey)
+  }
+
+  async _open () {}
+
+  async _close () {
+    await this.rpcClient.close()
   }
 
   async deleteService (publicKey) {
+    if (!this.opened) await this.ready()
+
     publicKey = IdEnc.decode(publicKey)
-    await this._makeRequest(
+    await this.rpcClient.makeRequest(
+      this.serverKey,
       'delete-service',
       { publicKey },
-      { requestEncoding: DeleteServiceRequest, responseEncoding: c.none }
+      { requestEncoding: DeleteServiceRequest, responseEncoding: cenc.none }
     )
   }
 }
--- client/register.js
+++ client/register.js
@@ -1,24 +1,36 @@
-const c = require('compact-encoding')
+const cenc = require('compact-encoding')
 const IdEnc = require('hypercore-id-encoding')
 const ProtomuxRpcClient = require('protomux-rpc-client')
+const ReadyResource = require('ready-resource')
 
 const { resolveStruct } = require('../spec/hyperschema')
 const HyperDHT = require('hyperdht')
 const PutServiceRequest = resolveStruct('@autodiscovery/put-service-request')
 
-class RpcDiscoveryRegisterClient extends ProtomuxRpcClient {
-  constructor (serverKey, dht, accessSeed, opts = {}) {
-    const keyPair = HyperDHT.keyPair(IdEnc.decode(accessSeed))
+class RpcDiscoveryRegisterClient extends ReadyResource {
+  constructor (serverKey, dht, accessSeed) {
+    super()
 
-    super(serverKey, dht, { ...opts, keyPair })
+    this.keyPair = HyperDHT.keyPair(IdEnc.decode(accessSeed))
+    this.rpcClient = new ProtomuxRpcClient(dht, { keyPair: this.keyPair })
+    this.serverKey = IdEnc.decode(serverKey)
+  }
+
+  async _open () {}
+
+  async _close () {
+    await this.rpcClient.close()
   }
 
   async putService (publicKey, service) {
+    if (!this.opened) await this.ready()
+
     publicKey = IdEnc.decode(publicKey)
-    await this._makeRequest(
+    await this.rpcClient.makeRequest(
+      this.serverKey,
       'put-service',
       { publicKey, service },
-      { requestEncoding: PutServiceRequest, responseEncoding: c.none }
+      { requestEncoding: PutServiceRequest, responseEncoding: cenc.none }
     )
   }
 }
